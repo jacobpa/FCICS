579aa5adac125e9cf427d2bc36a4644fbd54f448
Jacob Patterson <jacob@jacobpa.com>
2020-04-03 16:17:05 -0400

    Update executable

--- bin/inmate-scrape.js
+++ bin/inmate-scrape.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const scraper = require('../lib/jails/scraper');
+const CovidClient = require('../lib/covid/client');
 
 (async () => {
     const inmates = await scraper.scrapeTotal();
@@ -9,4 +10,10 @@ const scraper = require('../lib/jails/scraper');
 (async() => {
     const inmatesByGender = await scraper.scrapeByGender();
     console.log(`There are ${inmatesByGender.male} male inmates and ${inmatesByGender.female} female inmates in Franklin County jails.`);
+})();
+
+(async() => {
+    const client = new CovidClient();
+    const cases = await client.get()
+    console.log(`There are ${cases.male} male and ${cases.female} female cases for a total of ${cases.total} total cases of COVID-19 in Franklin County.`);
 })();
