63af77594cf2e6c8fbef1100999110e2ca0429e0
Jacob Patterson <jacob@jacobpa.com>
2020-04-02 20:02:48 -0400

    Refactor scraper to use page class

--- src/scraper.js
+++ src/scraper.js
@@ -1,22 +1,16 @@
 const puppeteer = require('puppeteer');
-const bookingPage = require('./booking-page');
+const BookingPage = require('./page');
 
-const FCCBookingURL = 'https://fcsojmsweb.franklincountyohio.gov/Publicview/';
-
-(async () => {
+module.exports.scrape = async () => {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
-    await page.goto(FCCBookingURL);
+    const bp = new BookingPage(page);
 
-    await Promise.all([
-        page.waitForNavigation(),
-        page.click(bookingPage.searchButton)
-    ]);
-
-    let inmateRows = await page.$$(bookingPage.tableRow);
-    let inmates = await inmateRows.length;
+    await bp.open();
+    await bp.search()
+    const inmates = await bp.countInmates();
 
     console.log(`There appears to be ${inmates} total inmates`);
 
     await browser.close();
-})();
+}
